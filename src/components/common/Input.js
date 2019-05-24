89bd8a188c2f4d3a98e4f3230f7ef22a8079fb5b
prashant_yadav <prashantsingh.yadav@sadhan.co.in>
2019-05-24 16:56:44 +0530

    resolved autoCorrect issue for Android

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -11,7 +11,7 @@ const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => 
       <TextInput
         secureTextEntry={secureTextEntry}
         placeholder={placeholder}
-        autoCorrect="false"
+        autoCorrect={false}
         style={inputStyle}
         value={value}
         onChangeText={onChangeText}
